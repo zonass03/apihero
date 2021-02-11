1204dd2b1255e23f54c968a0891bbb838d24bf94
zonass03 <78787095+zonass03@users.noreply.github.com>
2021-02-10 21:22:45 -0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -71,7 +71,8 @@ class HomeController extends Controller
 
        // $response = Http::get('https://api.lazada.com.ph/rest/orders/get?sort_direction=DESC&offset=0&created_after=2021-02-10T00%3A00%3A00%2B08%3A00&sort_by=updated_at&app_key=125715&sign_method=sha256&timestamp=1612940288606&access_token=50000500119cd0xTpCgxxeSinmpYmtA11d68a91a5oTCMPcuxjBbFwBKVjxxxw8&sign=F7737C14FBAD201F975E26AF74D7FD9E7D8365E1CBAF8B34C48A3522C8FF4804');
 
-         return $this->request('/seller/get');
+          $request = new Request('GET', 'https://auth.lazada.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri=https://sellertest.herokuapp.com/'."&client_id=125715");
+                echo $request->getUri()->getPath(); 
        // return $response;
     }
     
